0b69f46214ee40fa663fd8cecd7d240540293ab3
gabedhiar <65133119+gabedhiar@users.noreply.github.com>
2023-12-03 12:28:22 +0700

    Update slackIntegration.js

--- api/slackIntegration.js
+++ api/slackIntegration.js
@@ -1,22 +1,17 @@
 // api/slackIntegration.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');  // Assuming you're using axios for making HTTP requests
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
-const OPENAI_API_KEY = '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';
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 app.post('/slack/integrate', async (req, res) => {
   try {
-    // Extract relevant data from the incoming Slack request
-    const { text, user_id } = req.body;
-
-    // Your ChatGPT integration logic here
+    const { text } = req.body;
     const chatGptResponse = await generateChatGptResponse(text);
-
-    // Respond back to Slack
     res.json({ text: chatGptResponse });
   } catch (error) {
     console.error('Error:', error);
@@ -25,7 +20,6 @@ app.post('/slack/integrate', async (req, res) => {
 });
 
 async function generateChatGptResponse(inputText) {
-  // Make a request to the ChatGPT API
   const response = await axios.post(
     'https://api.openai.com/v1/chat/completions',
     {
@@ -40,7 +34,6 @@ async function generateChatGptResponse(inputText) {
     }
   );
 
-  // Extract and return the ChatGPT response
   return response.data.choices[0].message.content;
 }
 
